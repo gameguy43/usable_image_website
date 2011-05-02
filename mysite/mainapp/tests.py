1ebf0a583788d09b6413ea337474301300fa202d
Parker Phinney <parker.phinney@gmail.com>
2011-05-02 14:01:35 -0400

    tests now make sure that our known good metadata is displayed on our mirror page

--- mysite/mainapp/tests.py
+++ mysite/mainapp/tests.py
@@ -13,6 +13,30 @@ Replace these with more appropriate tests for your application.
 
 from django.test import TestCase
 
+def should_this_fields_data_be_on_the_page(field, data_schema):
+    if field in data_schema.their_fields:
+        return True
+    for resolution, resolution_data in data_schema.resolutions.items():
+        if resolution_data['url_column_name'] == field:
+            return True
+    return False
+
+def is_iterable(x):
+    return getattr(x, '__iter__', False)
+
+def flatten(x):
+    result = []
+    # if it's already flat, just return it
+    if not is_iterable(x):
+        return x
+    for el in x:
+#if isinstance(el, (list, tuple)):
+        if hasattr(el, "__iter__") and not isinstance(el, basestring):
+            result.extend(flatten(el))
+        else:
+            result.append(el)
+    return result
+
 class SimpleTest(TestCase):
     def setUp(self):
         self.scraper_name = 'fema'
@@ -30,5 +54,21 @@ class SimpleTest(TestCase):
             response = c.get(url)
             # assert that we successfully load a page
             self.assertEqual(response.status_code, 200)
-            # TODO: look for the known_good metadata as a substring of the contents of the page
-            #self.assertEqual()
+
+            # look for the known_good metadata as a substring of the contents of the page
+            # grab the metadata from the live db
+            metadata = self.scraper.get_image_metadata_dict(id)
+                
+            # based on this, assemble a flat list of things to look for
+            things_to_look_for = []
+            for key, value in metadata.items():
+                if should_this_fields_data_be_on_the_page(key, self.scraper.imglib.data_schema):
+                    value_flat = flatten(value)
+                    things_to_look_for.append(value_flat)
+                    # TODO: assert that the page contains value_flat
+            things_to_look_for = flatten(things_to_look_for)
+
+            # make sure we have everything on the list
+            for thing in things_to_look_for:
+                self.assertContains(response, thing)
+                
